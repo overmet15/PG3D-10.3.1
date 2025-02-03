37ed328ea45045ed40786c71b4fdd8b5881e20d4
overmet15 <metsocool675@gmail.com>
2025-02-03 21:18:54 +0900

    Ants probes

--- Assets/Scripts/Editor/RecoverTools.cs
+++ Assets/Scripts/Editor/RecoverTools.cs
@@ -1,8 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public static class RecoverTools
-{ 
+public class RecoverTools : EditorWindow
+{
+    public LightProbeGroup lightProbeComponent;
+    public LightProbes selectedProbes;
 
+    [MenuItem("RecoverTools/Tools")]
+
+    static void GetMe()
+    {
+        GetWindow<RecoverTools>();
+    }
+
+    void OnGUI()
+    {
+        CreditsDraw();
+        ProbeDraw();
+    }
+
+    void CreditsDraw()
+    {
+        EditorGUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Made By overmet15 (click here)", EditorStyles.centeredGreyMiniLabel)) Application.OpenURL("https://github.com/overmet15");
+
+        EditorGUILayout.EndHorizontal();
+    }
+
+    void ProbeDraw()
+    {
+        lightProbeComponent = (LightProbeGroup)EditorGUILayout.ObjectField(lightProbeComponent, typeof(LightProbeGroup), true);
+        selectedProbes = (LightProbes)EditorGUILayout.ObjectField(selectedProbes, typeof(LightProbes), false);
+
+        if (lightProbeComponent == null || selectedProbes == null) return;
+
+        if (GUILayout.Button("Patch Light Probes"))
+        {
+            if (lightProbeComponent == null || selectedProbes == null) return; // double check cuz ive seen unity not updating UI
+            lightProbeComponent.probePositions = selectedProbes.positions;
+        }
+    }
 }
